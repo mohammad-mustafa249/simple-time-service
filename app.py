0bf525c2b87bfefd2ec802b2442184e742f376ac
mohammad-mustafa249 <aim.mustafa249@gmail.com>
2025-05-22 18:47:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime, timezone
 
 app = Flask(__name__)
 
-@app.route('/')
-def time_service():
-    return jsonify({
-        "timestamp": datetime.now(timezone.utc).isoformat() + "Z",
-        "ip": request.remote_addr
-    })
+@app.route("/", methods=["GET"])
+def index():
+    timestamp = datetime.utcnow().isoformat() + "Z"
+    ip = request.remote_addr
+    return jsonify({"timestamp": timestamp, "ip": ip})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
